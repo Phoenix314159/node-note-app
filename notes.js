2829424f03e85e5b29d19d3cd5c9acf3ff9c857c
Phoenix314159 <derivative7117@live.com>
2019-04-04 14:56:24 -0600

    updated notes.js

--- notes.js
+++ notes.js
@@ -1,42 +1,42 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-  return 'your notes...'
-}
+module.exports = {
 
-const addNote = (title, body) => {
-  const notes = loadNotes()
-  const duplicateNotes = notes.filter(note => note.title === title)
-  if(duplicateNotes.length === 0) {
-    notes.push({title, body})
-    saveNotes(notes)
-    console.log(chalk.green('new note added'))
-  } else {
-    console.log(chalk.red('note title taken'))
-  }
+  addNote(title, body) {
+    const notes = this.loadNotes()
+    const duplicateNotes = notes.filter(note => note.title === title)
+    if(duplicateNotes.length === 0) {
+      notes.push({title, body})
+      this.saveNotes(notes)
+      console.log(chalk.green('new note added'))
+    } else {
+      console.log(chalk.red('note title taken'))
+    }
+  },
 
-}
+  removeNote(title) {
+    const notes = this.loadNotes()
+    const newNotes = notes.filter(note => note.title !== title)
+    if(notes.length > newNotes.length) {
+      console.log(chalk.green(`note ${title} removed`))
+      this.saveNotes(newNotes)
+    }else {
+      console.log(chalk.red(`no note found`))
+    }
+  },
 
-const removeNote = title => {
-  const notes = loadNotes()
-  const newNotes = notes.filter(note => note.title !== title)
-  if(notes.length > newNotes.length) {
-    console.log(chalk.green(`note ${title} removed`))
-    saveNotes(newNotes)
-  }else {
-    console.log(chalk.red(`no note found`))
-  }
-}
+  saveNotes: notes => fs.writeFileSync('notes.json', JSON.stringify(notes)),
 
-const saveNotes = notes => fs.writeFileSync('notes.json', JSON.stringify(notes))
-
-const loadNotes = () => {
-  try {
-    return JSON.parse(fs.readFileSync('notes.json').toString())
-  } catch (err) {
-    return [] //if note.json doesnt exist return an empty array
+  loadNotes: () => {
+    try {
+      return JSON.parse(fs.readFileSync('notes.json').toString())
+    } catch (err) {
+      return [] //if note.json doesnt exist return an empty array
+    }
   }
 }
 
-module.exports = {getNotes, addNote, removeNote}
+
+
+
